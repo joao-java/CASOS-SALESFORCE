1a637f8b36c907852aa7da435ba6f73250a0cf48
João melo <67669777+joao-java@users.noreply.github.com>
2023-09-14 17:07:18 -0300

    att

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 function mostrarOpcao() {
     const selecao = document.getElementById("chooseOption").value;
 
+
     const logistica = document.getElementById("logistica");
     const devolucaoMain = document.getElementById("devolucaoMain");
-    const MaterialAvariadoMain = document.getElementById("MaterialAvariadoMain");
+    const financeiro = document.getElementById("financeiro");
+    const renegociacao = document.getElementById("renegociacao");
+    const boleto = document.getElementById("boleto");
+    const qualidade = document.getElementById("qualidade");
+    const comercial = document.getElementById("comercial");
     
     logistica.style.display = "none";
     devolucaoMain.style.display = "none";
-    MaterialAvariadoMain.style.display = "none";
-
+    financeiro.style.display = "none";
+    renegociacao.style.display = "none";
+    boleto.style.display = "none";
+    qualidade.style.display = "none";
+    comercial.style.display = "none";
     
+    console.log(selecao)
     switch (selecao) {
         case '1':
             logistica.style.display = "block";
-            console.log(logistica.childNodes[3].value)
             if (logistica.childNodes[3].value === 'Logistica1') {
                 devolucaoMain.style.display ='block';
-                console.log(devolucaoMain.childNodes[3].value)
-
             }
+            
             break;
     
         case '2':
-
+            financeiro.style.display = "block";
+            if (financeiro.childNodes[3].value === 'financeiro2') {
+                renegociacao.style.display = 'block';
+            }else if (financeiro.childNodes[3].value === 'financeiro3'){
+                boleto.style.display = 'block';
+            }
             break;
 
         case '3':
@@ -46,6 +58,7 @@ function mostrarOpcao() {
 document.getElementById("chooseOption").addEventListener("change", mostrarOpcao);
 document.getElementById("logisticaS").addEventListener("change", mostrarOpcao);
 document.getElementById("devolucaoS").addEventListener("change", mostrarOpcao);
+document.getElementById("financeiroS").addEventListener("change", mostrarOpcao);
 
 // Chame a função inicialmente para configurar o formulário
 mostrarOpcao();
